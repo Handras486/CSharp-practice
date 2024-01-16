309f89519ddf5234ee7981e76123c900b5cfe9d5
András <hajduandras486@gmail.com>
2024-01-16 18:46:50 +0100

    Added project

---
+++ Practice code/Practice code/Leet 01 Two Sum.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_code
+{
+    public class Leet_01_Two_Sum
+    {
+        public static int[] TwoSumSimple(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    int temp = nums[i] + nums[j];
+                    if (temp == target)
+                    {
+                        return new int[] {i, j};
+                    }
+                }
+            }
+            return null;
+        }
+
+        public static int[] TwoSumComplex(int[] nums, int target)
+        {
+            Dictionary<int, int> numsDict = new Dictionary<int, int>();// = nums.ToDictionary(x => x);
+            for (int i = 0; i < nums.Length; i++)
+            {
+                numsDict[i] = nums[i];
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int needed = target - nums[i];
+
+                if (numsDict.Any(x => x.Value == needed && x.Key != i))
+                {
+                    KeyValuePair<int,int> pair = numsDict.FirstOrDefault(x => x.Value == needed && x.Key != i);
+                    return new int[] { i, pair.Key};
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Practice code/Practice code/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dictionary.DictExample();
+
+            int[] outputSimple = Leet_01_Two_Sum.TwoSumSimple(new int[] { 3, 2, 4 }, 6);
+            Console.WriteLine($"Simple: {outputSimple[0]} , {outputSimple[1]}");
+
+            int[] outputComplex = Leet_01_Two_Sum.TwoSumComplex(new int[] { 3,2,4 }, 6);
+            Console.WriteLine($"Complex: {outputComplex[0]} , {outputComplex[1]}");
+            Console.ReadLine();
+        }
+    }
+}
